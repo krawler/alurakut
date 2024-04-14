551fb002b208f809d06a800e1079b5e75e08f9f8
Rafael Augusto Ramos <august.rafael@gmail.com>
2024-04-14 14:52:03 -0300

    outras pastas e arquivos adicionados

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -1,13 +1,28 @@
 import { createGlobalStyle } from "styled-components";
+import { AlurakutStyles } from "@/lib/AluraCommons";
 
 const GlobalStyle = createGlobalStyle`
-  html,
+ 
+ *{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+ }
+ 
+ html,
   body {
     color: ${({ theme }) => theme.colors.primary};
     padding: 0;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: #D9E6F6;  
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
   }
 
   a {
@@ -18,6 +33,9 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
+
+  ${AlurakutStyles}
+
 `;
 
 export default GlobalStyle;
